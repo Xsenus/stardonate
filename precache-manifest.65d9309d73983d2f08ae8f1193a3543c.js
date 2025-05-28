196021d0679b2ba767ed34ab16aae4038408270d
XsenusWork <ilel@list.ru>
2025-05-28 17:24:53 +0700

    Updates

---
+++ precache-manifest.65d9309d73983d2f08ae8f1193a3543c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc67beb19b5625a4b4d71585452f78db",
+    "url": "/stardonate/index.html"
+  },
+  {
+    "revision": "f09c1e7e8d9852a018e3",
+    "url": "/stardonate/static/css/main.6ecb2e0f.chunk.css"
+  },
+  {
+    "revision": "2b3606541889b569994e",
+    "url": "/stardonate/static/js/2.7a62a0a8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/stardonate/static/js/2.7a62a0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09c1e7e8d9852a018e3",
+    "url": "/stardonate/static/js/main.b391d4c3.chunk.js"
+  },
+  {
+    "revision": "8ec62f587fbcfdf19708",
+    "url": "/stardonate/static/js/runtime-main.e706b38c.js"
+  }
+]);
